58054020a37b2c9370f9e8e0175b7ef2737c2ed9
Islam <im.islam94@gmail.com>
2022-02-11 07:49:17 +0200

    structuring

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Container} from '../../components/container';
+import {styles} from './styles';
+
+export const Home = () => {
+  return (
+    <Container>
+      <Text>Hello World!</Text>
+    </Container>
+  );
+};
